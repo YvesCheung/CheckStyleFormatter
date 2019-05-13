12b087203b17a9a18e0b7bf607de0237a790c879
pengyangfan <954078940@qq.com>
2019-05-13 16:09:50 +0800

    add  test case

--- reformat-core/src/test/java/com/unionyy/mobile/reformat/core/LineBreakerTest.kt
+++ reformat-core/src/test/java/com/unionyy/mobile/reformat/core/LineBreakerTest.kt
@@ -142,4 +142,86 @@ public class A {
 }
         """.trimIndent())
     }
+
+    @Test
+    fun testJavaFunctionCallTooLong() {
+        val text = CodeFormatter.reformat("C.java", """
+package com.yy.mobile.checkstyleformatter;
+
+public class A {
+
+    public A(String arg1, int arg2, String veryLongArg3, float arg4, String arg5, byte[] arg6, int arg7, String veryLongArg8) {
+        System.out.println(veryLongArg3);
+    }
+
+    public Observable<LoadPluginListener.Result> loadPlugin(final SinglePluginInfo pluginInfo, final boolean showDefaultLoading) {
+        MLog.info("PluginCenterApiImpl", "loadPlugin plugin = %s, showDefaultLoading = %s", pluginInfo, showDefaultLoading);
+        return PluginCenterController.INSTANCE.loadPlugin(pluginInfo, showDefaultLoading);
+    }
+}
+        """.trimIndent(), setOf(DumpAST(), LineBreaker()))
+
+        Assert.assertEquals(text, """""".trimIndent())
+    }
+
+    @Test
+    fun testJavaStringTooLong() {
+        val text = CodeFormatter.reformat("D.java", """
+package com.yy.mobile.checkstyleformatter;
+
+public class A {
+
+    public A(String arg1, int arg2, String veryLongArg3, float arg4, String arg5, byte[] arg6, int arg7, String veryLongArg8) {
+        System.out.println(veryLongArg3);
+    }
+
+    public Observable<LoadPluginListener.Result> loadPlugin(final SinglePluginInfo pluginInfo, final boolean showDefaultLoading) {
+        MLog.info("zhangyu4 is a nice man, wangfeihang is a beautiful woman, pengkangjia is a well guider, pengyangfan is a good xiaodi");
+    }
+}
+        """.trimIndent(), setOf(DumpAST(), LineBreaker()))
+
+        Assert.assertEquals(text, """""".trimIndent())
+    }
+
+    @Test
+    fun testJavaConditionTooLong() {
+        val text = CodeFormatter.reformat("D.java", """
+package com.yy.mobile.checkstyleformatter;
+
+public class A {
+
+    public BasePluginViewController getCurrentUsingController(boolean isHost) {
+        for (Map.Entry<String, WeakReference<BasePluginViewController>> entry : isHost ? mHostControllers.entrySet() : mGuestControllers.entrySet()) {
+            if (pluginsReadyState.containsKey(entry.getKey())) {
+                return entry.getValue().get();
+            }
+        }
+
+        return null;
+    }
+
+    public Observable<LoadPluginListener.Result> loadPlugin(final SinglePluginInfo pluginInfo, final boolean showDefaultLoading) {
+        if (LiveModuleManagerProxy.getInstance().isBookMode(EarningRxEvent.StreamLight_Type) || LiveModuleManagerProxy.getInstance().getEarnigCommonEvent() != null) {
+            return null;
+        }
+    }
+
+    public boolean isPluginLianMai() {
+        int lianMaiType = ICoreManagerBase.getCore(ITransChannelLianMaiCore.class).getLianmaiType().getZhangyu4().getWangfeihang();
+        if (lianMaiType == TransChannelLianMaiType.HAPPY_BASKETBALL || lianMaiType == TransChannelLianMaiType.GREEDY_FACE || lianMaiType == TransChannelLianMaiType.FACE_LIMINATE
+//                || lianMaiType == TransChannelLianMaiType.PVP
+        ) {
+            MLog.info(TAG, "ICoreManagerBase.getCore(ITransChannelLianMaiCore.class).isPluginLianMai : true");
+            return true;
+        }
+        MLog.info(TAG, "ICoreManagerBase.getCore(ITransChannelLianMaiCore.class).isPluginLianMai : false");
+        AudienceMetadataManager manager = AudienceMetadataManager.getInstance();
+        return manager.isBasketBallLianMai() || manager.isGreedyFaceLianMai() || manager.isOppositeScoreLianMai();
+    }
+}
+        """.trimIndent(), setOf(DumpAST(), LineBreaker()))
+
+        Assert.assertEquals(text, """""".trimIndent())
+    }
 }
